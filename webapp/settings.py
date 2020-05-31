5966756c6c257ebf14a9d5128e3634d6af38689c
Derrick Bol <derrickbol33@gmail.com>
2020-06-01 01:01:32 +0800

    setting

--- webapp/settings.py
+++ webapp/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'localhost',
+                 '.amazonaws.com',
+                 '.elasticbeanstalk.com']
 
 AUTHENTICATION_BACKENDS = [
     'djwarrant.backend.CognitoBackend',
